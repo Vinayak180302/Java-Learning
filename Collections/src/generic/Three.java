6226d7f89cde257c0705eb588c8ae974295cc234
Vinayak180302 <vinayakkumkar2002@gmail.com>
2025-11-02 21:50:05 +0530

    generic example

---
+++ Collections/src/generic/Three.java
@@ -0,0 +1,34 @@
+package generic;
+
+public class Three {
+
+	public static void main(String[] args) {
+			Iadd1<Integer> i1 = new Addition1();
+			
+			i1.add(10, 20);
+			
+			
+			
+		 
+
+	}
+
+}
+
+interface Iadd1<T>{
+	
+   public void add(T t1, T t2);
+	
+}
+
+class Addition1 implements Iadd1<Integer>{
+
+	@Override
+	public void add(Integer t1, Integer t2) {
+		int result = t1 + t2;
+		System.out.println("addition is " + result);
+		
+	}
+	
+	
+}
