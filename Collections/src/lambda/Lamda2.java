d0537538a16b1f28f39db7fb945d553372c34552
Vinayak180302 <vinayakkumkar2002@gmail.com>
2025-11-23 23:27:19 +0530

    Lamda exp.2

--- Collections/src/lambda/Lamda2.java
+++ Collections/src/lambda/Lamda2.java
@@ -7,8 +7,10 @@ public class Lamda2 {
 		I_Name<String> nam1 = (i, j)->{return "Hello! " + i + j ;};
 		System.out.println(nam1.name("Vinayak", "kumkar"));
 		
+		
 		I_Name<String> nam2 = (i, j)->{return i + "is my name and " + j + " is my family name";};
 		System.out.println(nam2.name("Vinayak", "Kumkar"));
+		
 		
 		I_Name<Integer> nam3 = (i, j)->{
 			if(i % 2 == 0)return 1;
@@ -17,6 +19,7 @@ public class Lamda2 {
 		
 		System.out.println(nam3.name(11, null));
 		
+		
 		I_Name<Integer> nam4 = (i, j)->{
 			if(i % 2 == 0 && j % 2 == 0)return  1; 
 			return 0;
@@ -24,12 +27,14 @@ public class Lamda2 {
 		
 		System.out.println(nam4.name(12, 24));
 		
+		
 		I_Name<String> nam5 = (i, j)->{
 			if(i.length() > 2)return "length is greater than 2";
 			return "length is less than 2";
 		};
 		
 		System.out.println(nam5.name("vinayak", null));
+		
 		
 		I_Name<String> nam6 = (i, j)->{
 			if(i.length() == j.length()) {
@@ -42,6 +47,7 @@ public class Lamda2 {
    } 
 
 }
+
 
 
 interface I_Name<T> {
