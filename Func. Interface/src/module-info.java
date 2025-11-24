cbc8f6b9368a60cc7c194cd85a4ec4d2216c1e9f
Vinayak180302 <vinayakkumkar2002@gmail.com>
2025-11-24 19:51:45 +0530

    Predicate<T> - func. interface

---
+++ Func. Interface/src/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module Func.Interface {
+}
---
+++ Func. Interface/src/predicate/First.java
@@ -0,0 +1,47 @@
+package predicate;
+
+import java.util.function.Predicate;
+
+/*
+ * Predicate<T t> - 
+ * - it is func. interface
+ * - Predicate<T> is used to check the specific condition
+ * 
+ * Methods in Predicate<T> -
+ * 1)  -test(T t);    // abstract method
+ *   -return true if input arg. matches the predicate, otherwise false
+ *  
+ * 2)  -default Predicate<T> negate() - 
+ *   -return reverse of the result of test() method
+ *   -in short, return predicate that represent logical negation of given predicate
+ *   
+ * 3)  -default Predicate<T> and(Predicate<T>)- 
+ *   - works on 2 predicate 
+ *   - returns true, if both predicates are true
+ *  
+ * 4)  -default Predicate<T> or(Predicate<T>)-
+ *   - works on 2 predicate
+ *   - returns true, if even only one condition is true 
+ *   - 
+ * 
+ * * */
+public class First {
+
+	public static void main(String[] args) {
+		
+		Predicate<Integer> pred = (i)->{return i > 10;};
+		
+		Predicate<Integer> pred2 = (j)->{return  j % 2 == 0;};
+		
+		System.out.println(pred.test(15));
+		
+		System.out.println(pred.negate().test(15));
+		
+		Predicate<Integer> pred3 = pred.and(pred2);
+		System.out.println(pred3.test(-1));
+		
+		Predicate<Integer> pred4 = pred.or(pred2);
+		System.out.println(pred4.test(15));
+	} 
+
+}
